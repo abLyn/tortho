eaae1ecce9896df3a401152239d2225f4b9d749c
Chaffa <dr.chaffa@gmail.com>
2023-10-16 03:00:40 +0100

    adding classnames

--- components/leftSidebar/Links.tsx
+++ components/leftSidebar/Links.tsx
@@ -3,20 +3,24 @@ import { sidebarLinks } from '@/constants'
 import Link from 'next/link'
 import React from 'react'
 import { usePathname } from 'next/navigation'
-
+import classnames from 'classnames'
 const Links = () => {
-  const pathname = usePathname()
+  const currentPath = usePathname()
   return (
     <div className="flex w-full flex-1 flex-col gap-6 px-6 ">
       {sidebarLinks.map((link) => {
         const isActive =
           //(pathname.includes(link.route) && link.route.length > 1) ||
-          pathname === link.route
+          currentPath === link.route
         return (
           <Link
             key={link.label}
             href={link.route}
-            className={`leftsidebar_link ${isActive && 'bg-muted '}`}
+            //className={`leftsidebar_link ${isActive && 'bg-muted '}`}
+            className={classnames({
+              leftsidebar_link: true,
+              'bg-muted ': isActive,
+            })}
           >
             {link.icon}
 
