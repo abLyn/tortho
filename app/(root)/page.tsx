cf2229b193f878362cab377913ec343ce4c9d226
Chaffa <dr.chaffa@gmail.com>
2023-10-26 02:36:02 +0100

    implement pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,17 @@
-import SearchBar from '@/components/SearchBar'
-import { Button } from '@/components/ui/button'
 import Pagination from '../../components/Pagination'
 
-const Home = () => {
+const Home = ({ searchParams }: { searchParams: { page: string } }) => {
   return (
     <>
       <h1 className=" text-4xl custom-scrollbar font-extrabold tracking-tight lg:text-5xl mb-5">
         Bienvenu!
       </h1>
-
       <p>Have a nice day</p>
-
-      <Pagination itemCount={100} pageSize={10} currentPage={5} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </>
   )
 }
--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Button } from './ui/button'
 import {
@@ -6,6 +8,7 @@ import {
   ChevronsLeftIcon,
   ChevronsRightIcon,
 } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 interface Props {
   itemCount: number
@@ -14,24 +17,49 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const pageCount = Math.ceil(itemCount / pageSize)
   if (pageCount <= 1) return null
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', page.toString())
+    router.push('?' + params.toString())
+  }
 
   return (
     <div className="flex  place-items-center gap-2 ">
       <p>
         Page {currentPage} of {pageCount}
       </p>
-      <Button variant="ghost" disabled={currentPage === 1}>
+      <Button
+        variant="ghost"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <ChevronsLeftIcon />
       </Button>
-      <Button variant="ghost" disabled={currentPage === 1}>
+      <Button
+        variant="ghost"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button variant="ghost" disabled={currentPage === pageCount}>
+      <Button
+        variant="ghost"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button variant="ghost" disabled={currentPage === pageCount}>
+      <Button
+        variant="ghost"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <ChevronsRightIcon />
       </Button>
     </div>
