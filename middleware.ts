a252013360a47aa757e1dd7afeb144256a4eb3c3
Chaffa <dr.chaffa@gmail.com>
2023-11-05 08:43:16 +0100

    restrict register user

--- middleware.ts
+++ middleware.ts
@@ -15,6 +15,13 @@ export default withAuth(
     }
 
     if (
+      request.nextUrl.pathname.startsWith('/register') &&
+      request.nextauth.token?.role !== 'Admin'
+    ) {
+      return NextResponse.rewrite(new URL('/denied', request.url))
+    }
+
+    if (
       request.nextUrl.pathname.startsWith('/client') &&
       request.nextauth.token?.role !== 'Admin' &&
       request.nextauth.token?.role !== 'Doctor'
@@ -31,4 +38,4 @@ export default withAuth(
 
 // Applies next-auth only to matching routes - can be regex
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = { matcher: ['/extra', '/client', '/dashboard'] }
+export const config = { matcher: ['/register', '/client', '/dashboard'] }
